9a2487402822977025f5d82c59bb484e94f1b2d8
Anupam Dutta <adutta@constructiveloans.com>
2025-10-25 01:16:48 +0530

    End of section 2

---
+++ API/Controllers/MembersController.cs
@@ -0,0 +1,31 @@
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembersController(AppDbContext appDbContext) : ControllerBase
+    {
+        private readonly AppDbContext appDbContext = appDbContext;
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMember()
+        {
+            var members = await appDbContext.Users.ToListAsync();
+            return members;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetMember(string id)
+        {
+            var member = await appDbContext.Users.FindAsync(id);
+            if (member == null) return NotFound();
+            return member;
+        }
+    }
+}
